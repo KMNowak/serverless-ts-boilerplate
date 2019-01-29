13f7590b9c091377c82e5391256acaf8a2b7254d
Krzysztof Nowak <krzysztofmareknowak@gmail.com>
2019-01-29 11:26:23 +0100

    Initial comit

---
+++ src/services/messages/createChatMessage.ts
@@ -0,0 +1,18 @@
+import { APIGatewayEvent, Handler } from 'aws-lambda';
+
+export const handler: Handler = async (event: APIGatewayEvent) => {
+  const { httpMethod, path, queryStringParameters, pathParameters, body } = event
+    const response = {
+      statusCode: 201,
+      body: JSON.stringify({
+        message: 'createChatMessage',
+        method: httpMethod,
+        path,
+        query: queryStringParameters,
+        params: pathParameters,
+        body
+      })
+    }
+
+    return response
+  }
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals')
+const glob = require('glob')
+
+const getTSFileExtensionsRegex = /\.\/|.ts/g
+const getFileName = name => name.replace(getTSFileExtensionsRegex, '')
+const files = glob.sync('./*.ts')
+const entries = files.reduce((acc, file) => Object.assign(acc, {
+  [getFileName(file)]: file
+}), {})
+
+Object.keys(slsw.lib.entries).forEach(
+  key => (entries[key] = ['./src/scripts/source-map-install.js', slsw.lib.entries[key]])
+)
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: entries,
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+    ]
+  }
+}
